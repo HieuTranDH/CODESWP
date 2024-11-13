85db0a2811a3979fbed866fb0857e4520edce41f
tuongvyne <tuongvy01238@gmail.com>
2024-11-13 22:04:50 +0700

    impove response login

--- src/main/java/control/Customer_lostAccount.java
+++ src/main/java/control/Customer_lostAccount.java
@@ -21,41 +21,6 @@ import static util.Email.sendEmail;
 @WebServlet(name = "Customer_lostAccount", urlPatterns = {"/lostaccount"})
 public class Customer_lostAccount extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet Customer_lostAccount</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet Customer_lostAccount at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -63,14 +28,7 @@ public class Customer_lostAccount extends HttpServlet {
         request.getRequestDispatcher("/auth/lost-account.jsp").forward(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -78,17 +36,17 @@ public class Customer_lostAccount extends HttpServlet {
         String email = request.getParameter("identify");
         Customer_DB userDB = new Customer_DB();
         String msg;
-        Customer u = null;
+        Customer users = null;
         ArrayList<Customer> userlist = userDB.getAllCustomers();
         boolean checkMail = false;
         for (Customer us : userlist) {
             if (us.getEmail().equals(email)) {
-                u = us;
+                users = us;
                 checkMail = true;
                 break;
             }
         }
-        if (checkMail == true) {
+        if (checkMail) {
             int x = new Random().nextInt(90000) + 10000;
             sendEmail(email, x);
             request.setAttribute("x", x);
@@ -96,20 +54,11 @@ public class Customer_lostAccount extends HttpServlet {
             request.setAttribute("status", "lostaccount");
             request.getRequestDispatcher("/auth/verifyemail.jsp").forward(request, response);
         } else {
-            msg = "Email not found!";
+            msg = "Email not found. Please try again!";
             request.setAttribute("message", msg);
             request.getRequestDispatcher("/auth/lost-account.jsp").forward(request, response);
         }
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
 }
