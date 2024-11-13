d6bcf22ec25410e5dd1a9096d90abc195b1a3a57
tuongvyne <tuongvy01238@gmail.com>
2024-11-13 21:52:49 +0700

    noti update transfer eng

--- src/main/java/control/Admin_movieManagement.java
+++ src/main/java/control/Admin_movieManagement.java
@@ -46,15 +46,7 @@ public class Admin_movieManagement extends HttpServlet {
         }
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -64,14 +56,7 @@ public class Admin_movieManagement extends HttpServlet {
         request.getRequestDispatcher("/staff/dashboards-movieMn.jsp").forward(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String action = request.getParameter("action");
@@ -206,25 +191,18 @@ public class Admin_movieManagement extends HttpServlet {
             String fileName = getFileName(filePart);
             filePart.write(uploadFilePath + File.separator + fileName);
 
-            // Cập nhật đường dẫn đến poster mới
             poster = "uploads/" + fileName;
         }
 
-        // Mặc định status là "Coming soon" nếu không được nhập từ form
         String status = request.getParameter("status") != null ? request.getParameter("status") : "Coming soon";
 
-        // Tạo đối tượng Movie từ dữ liệu form
         Movie movie = new Movie(movieId, title, duration, genre, formattedReleaseDate, description, status, poster, 0.00);
 
-        // Gọi phương thức updateMovie từ DAO/service để cập nhật phim
         boolean isUpdated = Staff_DB.updateMovie(movie);
 
-        // Kiểm tra xem quá trình cập nhật có thành công không
-        if (isUpdated) {
-            // Nếu cập nhật thành công, chuyển hướng về trang quản lý phim
-            request.getSession().setAttribute("msg", "Update bộ phim Success!");
+        if (isUpdated == true) {
+            request.getSession().setAttribute("msg", "Update bộ phim thành công!");
         } else {
-            // Nếu thất bại, chuyển hướng với thông báo lỗi
             request.getSession().setAttribute("msg", "Update bộ phim thất bại!");
         }
         response.sendRedirect(request.getContextPath() + "/staff/movieManagement");
@@ -234,14 +212,6 @@ public class Admin_movieManagement extends HttpServlet {
         // Logic xóa phim
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
+
 
 }
